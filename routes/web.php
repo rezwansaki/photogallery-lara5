ca80f6e97820d1d0c2398f63984212d1dc6022f4
rezwansaki <rezwansaki@gmail.com>
2021-12-22 00:22:33 +0600

    route test

--- routes/web.php
+++ routes/web.php
@@ -10,42 +10,41 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/','PhotoGallary@index'); // '/' means root. first run this route. 
+Route::get('/demo', function () {
+    return 'demo';
+});
+Route::get('/', 'PhotoGallary@index'); // '/' means root. first run this route. 
 
 Auth::routes();
 
-Route::get('/album/id/{id}','PhotoGallary@selectedAlbum');
-Route::get('/image/id/{id}','PhotoGallary@singleImage');
-Route::get('/search','PhotoGallary@search');
+Route::get('/album/id/{id}', 'PhotoGallary@selectedAlbum');
+Route::get('/image/id/{id}', 'PhotoGallary@singleImage');
+Route::get('/search', 'PhotoGallary@search');
 
 //admin controll 
 Route::get('/dashboard', 'PhotoGallary@dashboard');
-Route::get('/image/id/{id}/edit','PhotoGallary@edit');
-Route::post('/image/id/{id}/update','PhotoGallary@update');
-Route::get('/image/create','PhotoGallary@create');
-Route::post('/image/store','PhotoGallary@store');
-Route::delete('/image/id/{id}/destroy','PhotoGallary@destroy');
+Route::get('/image/id/{id}/edit', 'PhotoGallary@edit');
+Route::post('/image/id/{id}/update', 'PhotoGallary@update');
+Route::get('/image/create', 'PhotoGallary@create');
+Route::post('/image/store', 'PhotoGallary@store');
+Route::delete('/image/id/{id}/destroy', 'PhotoGallary@destroy');
 
-Route::delete('/admin/deleteallimages','PhotoGallary@deleteAllImages'); //delete all images at a time 
+Route::delete('/admin/deleteallimages', 'PhotoGallary@deleteAllImages'); //delete all images at a time 
 
-Route::get('/album/create','PhotoGallary@createalbum');
-Route::post('/album/store','PhotoGallary@storeAlbum');
-Route::get('/album/showAlbum','PhotoGallary@showAlbum');
-Route::get('/album/edit/{id}','PhotoGallary@editAlbum');
-Route::post('/album/update/{id}','PhotoGallary@updateAlbum');
-Route::delete('/album/id/{id}/destroy','PhotoGallary@destroyAlbum');
+Route::get('/album/create', 'PhotoGallary@createalbum');
+Route::post('/album/store', 'PhotoGallary@storeAlbum');
+Route::get('/album/showAlbum', 'PhotoGallary@showAlbum');
+Route::get('/album/edit/{id}', 'PhotoGallary@editAlbum');
+Route::post('/album/update/{id}', 'PhotoGallary@updateAlbum');
+Route::delete('/album/id/{id}/destroy', 'PhotoGallary@destroyAlbum');
 
 //profile 
 Route::get('/editProfile', 'PhotoGallary@editProfile');
 Route::post('/updateProfile', 'PhotoGallary@updateProfile');
 
 //admin - reset : reset the project as like at the first time - factory reset - to use new one 
-Route::delete('/admin/reset','PhotoGallary@resetApp');
+Route::delete('/admin/reset', 'PhotoGallary@resetApp');
 
 //admin - settings : such as project name, maximum uploaded file size, email for password reset system 
-Route::get('/admin/showSettings','PhotoGallary@showSettings'); //to show the settings blade page 
-Route::post('/admin/doSettings/{id}/update','PhotoGallary@doSettings'); //to update data from settings table 
-
-
-
+Route::get('/admin/showSettings', 'PhotoGallary@showSettings'); //to show the settings blade page 
+Route::post('/admin/doSettings/{id}/update', 'PhotoGallary@doSettings'); //to update data from settings table 
